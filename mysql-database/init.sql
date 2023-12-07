fb0687a0a181c2001f8482ce66de459f19ca9940
Gorun <307532500@qq.com>
2023-12-07 16:06:30 +0800

    Init repo

---
+++ mysql-database/init.sql
@@ -0,0 +1,10 @@
+﻿drop schema if exists stamp_market;
+create schema stamp_market;
+use stamp_market;
+
+create table user(
+    id int auto_increment primary key,
+    username varchar(32)
+);
+
+insert into user values (1, 'tom'), (2, 'jerry');
